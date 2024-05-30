8540c186fb180d3af7bce2b181d13d66b3a5920d
David Senoner <david.senoner@microtec.eu>
2024-05-30 05:45:34 +0200

    feat: increase SW version to 2.2.1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from app import MainWindow
 
-VERSION = "2.2.0"  # Version number
+VERSION = "2.2.1"  # Version number
 
 if __name__ == "__main__":
     if not QApplication.instance():
