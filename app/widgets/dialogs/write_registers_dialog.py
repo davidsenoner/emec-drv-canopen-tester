19995d318fef9594e23e0dd58c1423c1c2bf4dc7
David Senoner <david.senoner@microtec.eu>
2024-10-07 07:11:25 +0200

    feat: add methods to class for dialog management

---
+++ app/widgets/dialogs/write_registers_dialog.py
@@ -0,0 +1,65 @@
+import sys
+from PyQt5.QtWidgets import QDialog
+
+from app.widgets.ui_add_sn_diag import Ui_AddSNDialog
+from app.modules.drives.emec_canopen import TITAN40_EMECDRV5_SLEWING_NODE_ID, TITAN40_EMECDRV5_LIFT_NODE_ID, IO_DRIVE_SLEWING_ID
+
+
+class WriteRegistersDialog(QDialog):
+    def __init__(self, node_id: int, channel: int = 0, **kwargs):
+        """
+        Open Dialog with input fields for serial number, hardware and firmware version
+        :param node_id: Node ID of the selected drive
+        :param channel: CANOpen channel
+        :param kwargs: serial_number, hw_version, fw_version
+        """
+        super().__init__()
+
+        self._serial_number = kwargs.get("serial_number", 0)
+        self._hw_version = kwargs.get("hw_version", 0)
+        self._fw_version = kwargs.get("fw_version", 0)
+
+        self._ui = Ui_AddSNDialog()
+        self._ui.setupUi(self)
+
+        if self._serial_number == 0:
+            self._ui.led_serial_number.clear()
+        else:
+            self._ui.led_serial_number.setText(str(self._serial_number))
+
+        if self._hw_version == 0:
+            self._ui.led_hw_version.clear()
+        else:
+            self._ui.led_hw_version.setText(str(self._hw_version))
+
+        if self._fw_version == 0:
+            self._ui.led_fw_version.clear()
+        else:
+            self._ui.led_fw_version.setText(str(self._sw_version))
+
+        # Set lift node info to dialog
+        if node_id == TITAN40_EMECDRV5_LIFT_NODE_ID:
+            self._ui.lbl_drive_id.setText(f"<b>Lift</b> with Node ID: {node_id} on Channel: {channel}")
+
+        # Set slewing node info to dialog
+        elif node_id == TITAN40_EMECDRV5_SLEWING_NODE_ID:
+            self._ui.lbl_drive_id.setText(f"<b>Slewing</b> with Node ID: {node_id} on Channel: {channel}")
+
+        elif node_id == IO_DRIVE_SLEWING_ID:
+            self._ui.lbl_drive_id.setText(f"<b>Slewing</b> with IO controlled signals")
+
+        if self.exec_() == QDialog.Accepted:
+            check_sn = self._ui.led_serial_number.text()
+            if check_sn.isdigit():
+                self._serial_number = int(check_sn)
+            else:
+                self._serial_number = 0
+
+    def get_serial_number(self) -> int:
+        return self._serial_number
+
+    def get_hw_version(self) -> int | str:
+        return self._hw_version
+
+    def get_fw_version(self) -> int | str:
+        return self._fw_version
--- app/widgets/ui_add_sn_diag.py
+++ app/widgets/ui_add_sn_diag.py
@@ -1,29 +1,23 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'add_sn_diagkNbExu.ui'
+## Form generated from reading UI file 'add_sn_diagaPyfoc.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.5.1
+## Created by: Qt User Interface Compiler version 5.15.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PyQt5.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PyQt5.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
-    QGridLayout, QLabel, QLineEdit, QSizePolicy,
-    QVBoxLayout, QWidget)
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
 
 class Ui_AddSNDialog(object):
     def setupUi(self, AddSNDialog):
         if not AddSNDialog.objectName():
             AddSNDialog.setObjectName(u"AddSNDialog")
-        AddSNDialog.resize(400, 72)
+        AddSNDialog.resize(400, 163)
         self.verticalLayout = QVBoxLayout(AddSNDialog)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.lbl_drive_id = QLabel(AddSNDialog)
@@ -33,16 +27,41 @@ class Ui_AddSNDialog(object):
 
         self.gridLayout_2 = QGridLayout()
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.label_2 = QLabel(AddSNDialog)
-        self.label_2.setObjectName(u"label_2")
-
-        self.gridLayout_2.addWidget(self.label_2, 0, 0, 1, 1)
-
         self.led_serial_number = QLineEdit(AddSNDialog)
         self.led_serial_number.setObjectName(u"led_serial_number")
         self.led_serial_number.setMaxLength(50)
 
         self.gridLayout_2.addWidget(self.led_serial_number, 0, 1, 1, 1)
+
+        self.label_2 = QLabel(AddSNDialog)
+        self.label_2.setObjectName(u"label_2")
+
+        self.gridLayout_2.addWidget(self.label_2, 0, 0, 1, 1)
+
+        self.label_3 = QLabel(AddSNDialog)
+        self.label_3.setObjectName(u"label_3")
+
+        self.gridLayout_2.addWidget(self.label_3, 2, 0, 1, 1)
+
+        self.led_hw_version = QLineEdit(AddSNDialog)
+        self.led_hw_version.setObjectName(u"led_hw_version")
+
+        self.gridLayout_2.addWidget(self.led_hw_version, 1, 1, 1, 1)
+
+        self.label = QLabel(AddSNDialog)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout_2.addWidget(self.label, 1, 0, 1, 1)
+
+        self.led_fw_version = QLineEdit(AddSNDialog)
+        self.led_fw_version.setObjectName(u"led_fw_version")
+
+        self.gridLayout_2.addWidget(self.led_fw_version, 2, 1, 1, 1)
+
+        self.checkBox = QCheckBox(AddSNDialog)
+        self.checkBox.setObjectName(u"checkBox")
+
+        self.gridLayout_2.addWidget(self.checkBox, 3, 1, 1, 1)
 
 
         self.verticalLayout.addLayout(self.gridLayout_2)
@@ -66,5 +85,8 @@ class Ui_AddSNDialog(object):
         AddSNDialog.setWindowTitle(QCoreApplication.translate("AddSNDialog", u"Drive serial number", None))
         self.lbl_drive_id.setText(QCoreApplication.translate("AddSNDialog", u"TextLabel", None))
         self.label_2.setText(QCoreApplication.translate("AddSNDialog", u"Serial number", None))
+        self.label_3.setText(QCoreApplication.translate("AddSNDialog", u"FW Version", None))
+        self.label.setText(QCoreApplication.translate("AddSNDialog", u"HW Version", None))
+        self.checkBox.setText(QCoreApplication.translate("AddSNDialog", u"Write drive registers", None))
     # retranslateUi
 
